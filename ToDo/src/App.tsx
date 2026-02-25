a55959d902eb9253485f323d78c4fc1b7efadc4e
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-02-25 09:40:19 -0300

    Criando Componente Text com CVA

--- ToDo/src/App.tsx
+++ ToDo/src/App.tsx
@@ -1,3 +1,14 @@
+import Text from './components/text';
+
 export default function App() {
-    return <div className="text-pink-dark">Ola mundo</div>;
+    return (
+        <div className="flex flex-col gap-2">
+            <Text variant="body-sm-bold" className="text-pink-base">
+                Ola mundo!
+            </Text>
+            <Text className="text-green-base">Ola mundo!</Text>
+            <Text variant="body-md-bold">Ola mundo!</Text>
+            <Text variant="body-md-bold">Levar o dog pra passear</Text>
+        </div>
+    );
 }
