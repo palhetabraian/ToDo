eb35f78bf3f7d9f3e58be8c5cdefdce1a79a61b0
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-02-27 08:31:46 -0300

    Criando Componente Button

--- ToDo/src/App.tsx
+++ ToDo/src/App.tsx
@@ -7,6 +7,7 @@ import SpinnerIcon from './assets/icons/spinner.svg?react';
 import XIcon from './assets/icons/x.svg?react';
 import Icon from './components/icon';
 import Badge from './components/badge';
+import Button from './components/button';
 
 export default function App() {
     return (
@@ -31,6 +32,10 @@ export default function App() {
             <div>
                 <Badge variant="secondary">5</Badge>
                 <Badge variant="primary">2 de 5</Badge>
+            </div>
+
+            <div>
+                <Button icon={PlusIcon}>Nova tarefa</Button>
             </div>
         </div>
     );
