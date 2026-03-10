386b926ad42295f85f3f94120f39432d1c7c56cd
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-03-09 23:02:15 -0300

    Criando Componente da Tarefa

--- ToDo/src/pages/page-home.tsx
+++ ToDo/src/pages/page-home.tsx
@@ -1,4 +1,5 @@
 import Container from '../components/container';
+import TaskItem from '../core-components/task-item';
 import TasksSummary from '../core-components/task-summary';
 
 export default function PageHome() {
@@ -7,6 +8,8 @@ export default function PageHome() {
             <header className="flex items-center justify-between">
                 <TasksSummary />
             </header>
+
+            <TaskItem />
         </Container>
     );
 }
